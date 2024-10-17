5168b03dc12ae78cf98cdbcc43f0eafc0a3c5b40
Marcus Milantoni <Marcus.Milantoni@lhsc.on.ca>
2024-10-17 15:19:28 -0400

    add setup.cfg

---
+++ Slicer_Utils/__init__.py
@@ -0,0 +1,26 @@
+# Import classes
+
+from .image_volume import ImageVolume
+from .Segment import Segment
+from .segmentation_node import SegmentationNode
+from .utils import (
+    TempNodeManager, PresetColormaps, PetColormaps, volumeNodeTypes, 
+    check_type, log_and_raise, load_DICOM, create_volume_node, 
+    get_volume_nodes_by_type, sweep_screen_capture
+)
+
+__all__ = [
+    "ImageVolume",
+    "Segment",
+    "SegmentationNode",
+    "TempNodeManager",
+    "PresetColormaps",
+    "PetColormaps",
+    "volumeNodeTypes",
+    "check_type",
+    "log_and_raise",
+    "load_DICOM",
+    "create_volume_node",
+    "get_volume_nodes_by_type",
+    "sweep_screen_capture"
+]
