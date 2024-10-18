a0d2cd2883187e639f0b37020c22f537a6e4dfdd
Marcus Milantoni <Marcus.Milantoni@lhsc.on.ca>
2024-10-18 12:25:42 -0400

    version 0.1.1

--- app/slicerutil/__init__.py
+++ app/slicerutil/__init__.py
@@ -1,9 +1,9 @@
 # Import classes
 
-from .image_volume import ImageVolume
-from .Segment import Segment
-from .segmentation_node import SegmentationNode
-from .utils import (
+from src.image_volume import ImageVolume
+from src.Segment import Segment
+from src.segmentation_node import SegmentationNode
+from src.utils import (
     TempNodeManager, PresetColormaps, PetColormaps, volumeNodeTypes, 
     check_type, log_and_raise, load_DICOM, create_volume_node, 
     get_volume_nodes_by_type, sweep_screen_capture
